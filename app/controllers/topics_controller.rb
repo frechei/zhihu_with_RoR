acb9e26a1faffbeef3b036b6cea5f0635ccdd881
frechei <1490409009@qq.com>
2016-06-18 13:53:44 +0800

    更改compiled assets

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,6 @@
 class TopicsController < ApplicationController
+	before_action :logged_in_user, only: [:topic]
+
 	def remove
 		@question = Question.find(params[:question_id])
 		@topic = Topic.find(params[:id])
@@ -23,4 +25,12 @@ class TopicsController < ApplicationController
 		@topics = current_user.followed_tp
 		render 'own_topics'
 	end
+
+	private
+
+	 	def logged_in_user
+      unless logged_in?
+        redirect_to login_url, notice: "请先登陆"
+      end
+    end
 end
